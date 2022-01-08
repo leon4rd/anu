753764f1aa816f23c59cb394cd6898e1681c6513
Leon4rd <38402513+leon4rd@users.noreply.github.com>
2022-01-09 00:36:44 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -6,7 +6,9 @@ cd /tmp/rom
 #time tar xf out.tar.gz
 #rm out.tar.gz
 source build/envsetup.sh
+export TARGET_USES_MKE2FS=true
 lunch aosp_onclite-userdebug
+export TARGET_USES_MKE2FS=true
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
